4e852fa7c5bd2bf467c1d9ee504a291ff4042dda
iamalexstewart <iamalexstewart@gmail.com>
2025-05-21 19:27:03 +0000

    Update dir name

--- laravel-vue-starter/idx-template.nix
+++ laravel-vue-starter/idx-template.nix
@@ -9,8 +9,8 @@
 			mkdir "$out"
       wget2 https://github.com/laravel/vue-starter-kit/archive/refs/heads/main.zip
       unzip main.zip
-      mv ${./vue-starter-kit}/* "$out"/
-      rm -rf ${./vue-starter-kit}
+      mv ${./vue-starter-kit-main}/* "$out"/
+      rm -rf ${./vue-starter-kit-main}
 			mkdir -p "$out"/.idx
   		cp ${./dev.nix} "$out"/.idx/dev.nix
     '';
