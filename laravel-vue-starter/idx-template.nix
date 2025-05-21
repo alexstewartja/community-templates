3e6e965961303134adf40cca3c2ed887067a3af5
iamalexstewart <iamalexstewart@gmail.com>
2025-05-21 19:16:03 +0000

    Relativize paths

--- laravel-vue-starter/idx-template.nix
+++ laravel-vue-starter/idx-template.nix
@@ -8,9 +8,9 @@
     bootstrap = ''
 			mkdir "$out"
       wget2 https://github.com/laravel/vue-starter-kit/archive/refs/heads/main.zip
-      unzip main.zip
-      mv vue-starter-kit/* "$out"/
-      rm -rf vue-starter-kit
+      unzip ./main.zip
+      mv ./vue-starter-kit/* "$out"/
+      rm -rf ${./vue-starter-kit}
 			mkdir -p "$out"/.idx
   		cp ${./dev.nix} "$out"/.idx/dev.nix
     '';
